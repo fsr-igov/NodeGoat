fb25b3a248e8470bd0547580d449b267703559d1
Zouhair Fouiguira <Zouhairfgra@gmail.com>
2026-01-28 19:12:39 +0100

    fix: refactor database connection and collection handling in db-reset script

--- artifacts/db-reset.js
+++ artifacts/db-reset.js
@@ -36,9 +36,9 @@ const USERS_TO_INSERT = [
         //"password" : "$2a$10$Tlx2cNv15M0Aia7wyItjsepeA8Y6PyBYaNdQqvpxkIUlcONf1ZHyq", // User2_123
     }];
 
-const tryDropCollection = (db, name) => {
+const tryDropCollection = (database, name) => {
     return new Promise((resolve, reject) => {
-        db.dropCollection(name, (err, data) => {
+        database.dropCollection(name, (err, data) => {
             if (!err) {
                 console.log(`Dropped collection: ${name}`);
             }
@@ -60,13 +60,15 @@ const parseResponse = (err, res, comm) => {
 
 
 // Starting here
-MongoClient.connect(db, (err, db) =>  {
+MongoClient.connect(db, (err, client) =>  {
     if (err) {
         console.log("ERROR: connect");
         console.log(JSON.stringify(err));
         process.exit(1);
     }
     console.log("Connected to the database");
+    
+    const database = client.db();
 
     const collectionNames = [
         "users",
@@ -78,13 +80,13 @@ MongoClient.connect(db, (err, db) =>  {
 
     // remove existing data (if any), we don't want to look for errors here
     console.log("Dropping existing collections");
-    const dropPromises = collectionNames.map((name) => tryDropCollection(db, name));
+    const dropPromises = collectionNames.map((name) => tryDropCollection(database, name));
 
     // Wait for all drops to finish (or fail) before continuing
     Promise.all(dropPromises).then(() => {
-        const usersCol = db.collection("users");
-        const allocationsCol = db.collection("allocations");
-        const countersCol = db.collection("counters");
+        const usersCol = database.collection("users");
+        const allocationsCol = database.collection("allocations");
+        const countersCol = database.collection("counters");
 
         // reset unique id counter
         countersCol.insert({
@@ -127,6 +129,7 @@ MongoClient.connect(db, (err, db) =>  {
             allocationsCol.insertMany(finalAllocations, (err, data) => {
                 parseResponse(err, data, "allocations.insertMany");
                 console.log("Database reset performed successfully");
+                client.close();
                 process.exit(0);
             });
 
